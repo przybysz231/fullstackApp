19f34387cc4c7d9afab3442495e06000b0107edd
przybysz231 <przybysz231@gmail.com>
2018-10-17 23:50:20 +0200

    third

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ var app = new express();
  app.get ('/',function(req, res){
  	res.render('./../app/index.ejs', {});
  })
- .use(express.static(__dirname + '/../app'))
+ .use(express.static(__dirname + '/../.temp'))
  .listen(7777);
  
 
